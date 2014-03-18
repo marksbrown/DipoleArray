ccbbec2adcf52549087437dcec86afbc5872e4ff
Mark S. Brown <zcicg94@ucl.ac.uk>
2014-03-18 21:19:06 +0000

    sanity updates to plot.py

--- dipolearray/plot.py
+++ dipolearray/plot.py
@@ -1,79 +1,37 @@
-'''
-Plotting functions for _dipolearray (requires matplotlib >= 1.1.0)
-'''
+"""
+Plotting functions for dipolearray (requires matplotlib >= 1.1.0)
+"""
 
 from __future__ import division, print_function
 from numpy import pi, ptp, min, max, array
 from numpy import invert, isnan
-import os
 from . import dipolearray as da
-from matplotlib.pyplot import cm
 
-
-# Constants
-nm = 1e-9
-Degrees = pi / 180
-
-
-def savefig(name, fig, verbose=0, **kwargs):
-    '''
-    Saves figure
-
-    kwargs:
-    imagesavelocation : imagesavelocation
-    ext : extensions to save as and into
-    '''
-
-    imagesavelocation = kwargs.get('imagesavelocation',
-                                   os.path.join(os.path.expanduser('~'), 'Desktop', 'images'))
-
-    if not os.path.exists(imagesavelocation):
-        if verbose > 1:
-            print("Creating ImageSaveLocation directory")
-        os.makedirs(imagesavelocation)
-
-    Ext = kwargs.get('ext', ['png', 'pdf', 'svg'])
-
-    for ext in Ext:
-        savetopath = os.path.join(imagesavelocation, ext)
-        if not os.path.exists(savetopath):
-            if verbose > 1:
-                print("Creating savetopath directory")
-            os.mkdir(savetopath)
-
-        saveto = os.path.join(savetopath, name + "." + ext)
-
-        if verbose > 0:
-            print("Saving image to", saveto)
-        fig.savefig(saveto)
-
-
-def PlotLattice(axis, lc, N1, N2, verbose=0, **kwargs):
-    '''
-    Plots given lattice on subplot using matplotlib
-
+def PlotLattice(axis, lc, N1, N2, **kwargs):
+    """
+    Plots given Bravais lattice
+    
+    ---args--
     axis : matplotlib subplot instance
-
-    lc : 2D bravais lattice  : (length-1,angle-1,length-2,angle-2)
-        example of square lattice [100*nm,0*Degrees,100*nm,90*Degrees]
-
+    lc : 2D bravais lattice  : (length-1, angle-1, length-2, angle-2)
+        e.g a square lattice can be [100*nm, 0*Degrees, 100*nm, 90*Degrees]
     N1, N2 : number of dipoles in first and second lattice axis
 
-    ---
-
-    kwargs
-
+    ---kwargs---
+    verbose : verbosity control
     dipolelength : Length of dipole
     dipoleangle : Angle with respect to cartesian x axis (phi)
     title : Title of plot
-    xlim, ylim : x, y limits
-    '''
+    limfactor : padding about dipole array to show
+    divideby : unit chosen - defaults to nm
+    """
 
-    DipoleLength = kwargs.get('dipolelength', 50)
-    DipoleAngle = 90 - kwargs.get('dipoleangle', 0)
-    Title = kwargs.get('title', "")
-    limfactor = kwargs.get('limfactor', 1.1)
-    divideby = kwargs.get('divideby', nm)
+    verbose = kwargs.pop('verbose', 0)
+    DipoleLength = kwargs.pop('dipolelength', 50)
+    DipoleAngle = 90 - kwargs.pop('dipoleangle', 0)
+    Title = kwargs.pop('title', "")
+    limfactor = kwargs.pop('limfactor', 1.1)
+    divideby = kwargs.pop('divideby', 1e-9)
 
     if verbose > 0:
         print("Number of Dipoles Generated", ptp(N1) * ptp(N2))
@@ -85,13 +43,15 @@ def PlotLattice(axis, lc, N1, N2, verbose=0, **kwargs):
 
     xlim = (min(X) - limfactor * min(X), max(X) + limfactor * min(X))
     ylim = (min(Y) - limfactor * min(Y), max(Y) + limfactor * min(Y))
-
-    axis.grid()
+    
+    ##If we pass a 3D object we will plot correctly implicitly
     try:
-        axis.scatter3D(X, Y, Z, marker=(2, 0, DipoleAngle), s=DipoleLength)
+        axis.scatter3D(X, Y, Z, marker=(2, 0, DipoleAngle),
+                         s=DipoleLength, **kwargs)
         axis.set_zlabel("Z Position (nm)")
     except AttributeError:
-        axis.scatter(X, Y, marker=(2, 0, DipoleAngle), s=DipoleLength)
+        axis.scatter(X, Y, marker=(2, 0, DipoleAngle), 
+                        s=DipoleLength, **kwargs)
 
     axis.set_xlabel("X Position (nm)")
     axis.set_ylabel("Y Position (nm)")
@@ -102,19 +62,22 @@ def PlotLattice(axis, lc, N1, N2, verbose=0, **kwargs):
     return axis
 
 
-def tocube(axis, anum=1):
-    axis.set_xlabel("x", size=20)
-    axis.set_ylabel("y", size=20)
-    axis.set_zlabel("z", size=20)
-    axis.set_xlim(-anum, anum)
-    axis.set_ylim(-anum, anum)
-    axis.set_zlim(-anum, anum)
+def tocube(axis, cubeedge=1, size=20):
+    """
+    Sets 3D axis to cube
+    """
+    axis.set_xlabel("x", size=size)
+    axis.set_ylabel("y", size=size)
+    axis.set_zlabel("z", size=size)
+    axis.set_xlim(-cubeedge, cubeedge)
+    axis.set_ylim(-cubeedge, cubeedge)
+    axis.set_zlim(-cubeedge, cubeedge)
 
 
 def fetchmaxnum(x, y, z):
-    '''
+    """
     Returns maximum dimension allowing us to scale sensibly
-    '''
+    """
     anumx = max(x[invert(isnan(x))])
     anumy = max(y[invert(isnan(y))])
     anumz = max(z[invert(isnan(z))])
@@ -122,27 +85,34 @@ def fetchmaxnum(x, y, z):
     return max([anumx, anumy, anumz])
 
 
-def GenerateFarFieldImage(n0, k, N1, N2, lc, p, steps, afig, axis, verbose=0, **kwargs):
-    '''
+def Farfield3DImage(n0, k, N1, N2, lc, p, axis, **kwargs):
+    """
     Generate 3D matplotlib (>= 1.30 required if you wish save the image as svg)
     image of farfield pattern. 3 faces are plotted with the respective direction
     cosines
-    '''
+
+    ---Args---
+    n0 - incident direction
+    k - wavenumber
+    N1, N2 : number of scatterers in x, y
+    p : dipole moment
+    figure : MatPlotlib figure
+    axis : MatPlotlib subplot instance
+
+    --Kwargs--
+    steps : number of discrete steps in the plot to err...plot
+    verbose : verbosity control
+    """
+    
+    steps = kwargs.pop('steps', 200)
+    verbose = kwargs.pop('verbose', 0)
+    
     theta, phi = da.AnglesofHemisphere('all', steps)
-    alldirections = da.DirectionVector(theta, phi)
-
-    N = kwargs.pop("N",50)
-    cmap = kwargs.pop("cmap",cm.hot)
-
+    alldirections = da.DirectionVector(theta, phi)    
     F = da.OutgoingDirections(alldirections, n0, N1, N2, lc, k)
 
-    dsdo = da.DifferentialCrossSection(
-        F,
-        n0,
-        alldirections,
-        p=p,
-        k=k,
-        const=False)
+    dsdo = da.DifferentialCrossSection(F, n0, alldirections, p=p,
+                                        k=k, const=False)
 
     adir = da.DirectionVector(theta, phi, dsdo)
     maxnum = max(adir)
@@ -154,145 +124,52 @@ def GenerateFarFieldImage(n0, k, N1, N2, lc, p, steps, afig, axis, verbose=0, **
 
     tocube(axis)
 
+    return axis
+
+def Farfield3DDirectionCosines(n0, k, N1, N2, lc, p, axis, **kwargs):
+    """
+    Generate 3D image of direction cosines of farfield pattern due to
+    dipole array.
+
+    ---Args---
+    n0 - incident direction
+    k - wavenumber
+    N1, N2 : number of scatterers in x, y
+    p : dipole moment
+    axis : MatPlotlib subplot instance
+
+    --Kwargs--
+    verbose : verbosity control
+    """
+    steps = kwargs.pop('steps', 200)
+    verbose = kwargs.pop('verbose', 0)
+    N = kwargs.pop("N",50)
+    
     for adir in ['x', 'y', 'z']:
 
         theta, phi = da.AnglesofHemisphere(adir, steps)
         alldirections = da.DirectionVector(theta, phi)
 
-        dsdo = da.DifferentialCrossSection(
-            F,
-            n0,
-            alldirections,
-            p=p,
-            k=k,
-            const=False)
+        F = da.OutgoingDirections(alldirections, n0, N1, N2, lc, k)
+                
+        dsdo = da.DifferentialCrossSection(F, n0, alldirections, 
+                                          p=p, k=k, const=False)
         dsdo /= max(dsdo)
 
         ux, uy = da.GetDirectionCosine(adir, steps)
 
         if adir == 'x':
-            ctf = axis.contourf(
-                dsdo,
-                ux,
-                uy,
-                N,
-                zdir=adir,
-                cmap=cmap,
-                offset=-1,
-                **kwargs)
+            ctf = axis.contourf(dsdo, ux, uy, N, 
+                zdir=adir, offset=-1, **kwargs)
         elif adir == 'y':
-            ctf = axis.contourf(
-                ux,
-                dsdo,
-                uy,
-                N,
-                zdir=adir,
-                cmap=cmap,
-                offset=1,
-                **kwargs)
+            ctf = axis.contourf(ux, dsdo, uy, N,
+                zdir=adir, offset=1, **kwargs)
         elif adir == 'z':
-            ctf = axis.contourf(
-                ux,
-                uy,
-                dsdo,
-                N,
-                zdir=adir,
-                cmap=cmap,
-                offset=-1,
-                **kwargs)
+            ctf = axis.contourf(ux, uy, dsdo, N,
+                zdir=adir, offset=-1, **kwargs)
 
-    cb = afig.colorbar(ctf, ax=axis, use_gridspec=True, shrink=0.5)
-    cb.set_label("Scaled Absolute Electric Field Squared", size=15)
+    cb = axis.figure.colorbar(ctf, ax=axis, use_gridspec=True, shrink=0.5)
+    cb.set_label("Scaled Absolute \nElectric Field Squared", size=15)
     cb.set_ticks([0, 0.25, 0.5, 0.75, 1])
 
-    afig.tight_layout()
-
-    return
-
-
-def FarFieldWithDipoleArray(n0, k, N1, N2, lc, p, steps, fig, ax1, ax2, ax3, verbose=0):
-    '''
-    Farfield plot (similar GenerateFarFieldImage) except we plot on three distinct axes
-    '''
-
-    theta, phi = da.AnglesofHemisphere('all', steps)
-    alldirections = da.DirectionVector(theta, phi)
-    F = da.OutgoingDirections(alldirections, n0, N1, N2, lc, k)
-
-    dsdo = da.DifferentialCrossSection(
-        F,
-        n0,
-        alldirections,
-        p=p,
-        k=k,
-        const=False)
-
-    adir = da.DirectionVector(theta, phi, dsdo)
-
-    x = adir[..., 0]
-    y = adir[..., 1]
-    z = adir[..., 2]
-
-    ax1.set_title(str(N1[1] - 1) + " x " + str(N2[1] - 1))
-
-    ax1.plot_surface(x, y, z, alpha=0.2)
-
-    tocube(ax1, max(adir))
-
-    dsdo = {'x': array(1), 'y': array(1), 'z': array(1)}
-
-    def getdsdo(adir):
-        theta, phi = da.AnglesofHemisphere(adir, steps)
-        alldirections = da.DirectionVector(theta, phi)
-        F = da.OutgoingDirections(alldirections, n0, N1, N2, lc, k)
-
-        return (
-            da.DifferentialCrossSection(
-                F,
-                n0,
-                alldirections,
-                p=p,
-                k=k,
-                const=False)
-        )
-
-    dsdo = {adir: getdsdo(adir) for adir in dsdo}
-
-    Maxdsdo = max([max(dsdo[adir]) for adir in dsdo])
-
-    ux, uy = da.GetDirectionCosine('x', steps)
-    ax2.contourf(
-        dsdo['x'] / Maxdsdo,
-        ux,
-        uy,
-        50,
-        zdir='x',
-        cmap=cm.hot,
-        offset=-1)
-    ux, uy = da.GetDirectionCosine('y', steps)
-    ax2.contourf(
-        ux,
-        dsdo['y'] / Maxdsdo,
-        uy,
-        50,
-        zdir='y',
-        cmap=cm.hot,
-        offset=1)
-    ux, uy = da.GetDirectionCosine('z', steps)
-    ctf = ax2.contourf(
-        ux,
-        uy,
-        dsdo['z'] / Maxdsdo,
-        50,
-        zdir='z',
-        cmap=cm.hot,
-        offset=-1)
-
-    cb = fig.colorbar(ctf, ax=ax2, use_gridspec=True, shrink=0.5)
-    cb.set_label("Scaled |E|^2", size=15)
-    tocube(ax2)
-
-    PlotLattice(ax3, lc, N1, N2, dipoleangle=0, dipolelength=50)
-    fig.tight_layout()
-
-    return
+    return axis
