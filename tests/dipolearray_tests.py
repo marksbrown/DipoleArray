52536f4641c5f70f48b72aa51cb092c36a3c53fd
Mark S. Brown <mbrown@ee.ucl.ac.uk>
2014-03-21 17:53:10 +0000

    added first test

--- tests/dipolearray_tests.py
+++ tests/dipolearray_tests.py
@@ -1,11 +1,24 @@
+from __future__ import division, print_function
 from nose.tools import *
 import dipolearray as da
+from itertools import combinations
+from numpy import linspace, pi, shape
+
 
 def setup():
-    print "Setup!"
+    pass
 
 def teardown():
-    print "Tear Down!"
+    pass
 
-def test_basic():
-    print "I ran!"
+def test_radial_direction_vector():
+    Degrees = pi / 180
+    for j, (a, b, c) in enumerate(combinations(2 * (0, 5 * Degrees, linspace(0, 180, 5) * Degrees), 3)):
+        adir = da.radial_direction_vector(a, b, c)
+
+
+def test_radial_direction_vector():
+    Degrees = pi / 180
+    for j, (a, b, c) in enumerate(combinations(2 * (0, 5 * Degrees, linspace(0, 180, 5) * Degrees), 3)):
+        adir = da.radial_direction_vector(a, b, c)
+
