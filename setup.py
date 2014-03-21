a1a2ee9cd10768ef552d37896db48de5b668a87c
Mark S. Brown <mbrown@ee.ucl.ac.uk>
2014-03-21 16:10:19 +0000

    Added setup.py to project

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='phased_scatterer_array',
+    version='0.1',
+    packages=['', 'dipolearray'],
+    url='',
+    license='CRAPL',
+    author='Mark S. Brown',
+    author_email='contact@markbrown.io',
+    description='The differential cross section for a series of dipoles induced within a periodic array of scatterers is calculated'
+)
