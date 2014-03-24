7e58b10eae8095493bd3eb81e6d5765694fcb4c2
Mark S. Brown <mbrown@ee.ucl.ac.uk>
2014-03-24 14:43:35 +0000

    two valid test for unit vectors using numpy.test added

--- tests/dipolearray_tests.py
+++ tests/dipolearray_tests.py
@@ -2,7 +2,8 @@ from __future__ import division, print_function
 from nose.tools import *
 import dipolearray as da
 from itertools import combinations
-from numpy import linspace, pi, shape
+from numpy import linspace, pi, shape, cross
+from numpy.testing import assert_allclose
 
 
 def setup():
@@ -11,14 +12,24 @@ def setup():
 def teardown():
     pass
 
-def test_radial_direction_vector():
+def test_spherical_unit_vectors():
+    """
+    Test A) Does the function accept all predictable inputs?
+    Test B) Are the outputs always orthogonal?
+    """
     Degrees = pi / 180
-    for j, (a, b, c) in enumerate(combinations(2 * (0, 5 * Degrees, linspace(0, 180, 5) * Degrees), 3)):
-        adir = da.radial_direction_vector(a, b, c)
+    for a, b in combinations(2 * (0, 5 * Degrees, linspace(0, 180, 5) * Degrees), 2):
+        r, theta, phi = da.spherical_unit_vectors(a, b)
+        assert_allclose(cross(r, theta), phi, err_msg='vectors should be orthogonal')
+
+
 
 
 def test_radial_direction_vector():
+    """
+    Test A) Does the function accept all predictable inputs?
+    """
     Degrees = pi / 180
-    for j, (a, b, c) in enumerate(combinations(2 * (0, 5 * Degrees, linspace(0, 180, 5) * Degrees), 3)):
+    for a, b, c in combinations(2 * (0, 5 * Degrees, linspace(0, 180, 5) * Degrees), 3):
         adir = da.radial_direction_vector(a, b, c)
 
